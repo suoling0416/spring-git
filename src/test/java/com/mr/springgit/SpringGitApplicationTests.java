aaeb3b1d4627f0e8d74139d725fd62d282c5c255
suoling <suoll416@163.com>
2020-04-18 19:56:33 +0800

    提交代码

---
+++ src/test/java/com/mr/springgit/SpringGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr.springgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
