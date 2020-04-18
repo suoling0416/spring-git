8cc5dbc031540061a6d3910a0e5cf29422ff41d3
suoling0416 <63909512+suoling0416@users.noreply.github.com>
2020-04-18 20:42:14 +0800

    sdds

--- src/main/java/com/mr/springgit/SpringGitApplication.java
+++ src/main/java/com/mr/springgit/SpringGitApplication.java
@@ -8,6 +8,7 @@ public class SpringGitApplication {
 
     public static void main(String[] args) {
         system.out.print("索玲玲笨蛋");
+        System.out.print("索玲玲笨蛋");
         SpringApplication.run(SpringGitApplication.class, args);
     }
 
