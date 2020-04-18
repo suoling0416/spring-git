bc7f362519263371aad26396a93d549a0c9caf14
suoling <suoll416@163.com>
2020-04-18 20:45:11 +0800

    提交冲突1

--- src/main/java/com/mr/springgit/SpringGitApplication.java
+++ src/main/java/com/mr/springgit/SpringGitApplication.java
@@ -9,6 +9,7 @@ public class SpringGitApplication {
     public static void main(String[] args) {
         System.out.print("索玲玲笨蛋");
         System.out.print("索玲玲笨蛋");
+        System.out.print("索玲玲笨蛋");
         SpringApplication.run(SpringGitApplication.class, args);
     }
 
